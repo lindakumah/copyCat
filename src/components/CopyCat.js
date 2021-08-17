d7802055ba3fe2229299c4950adadf00256b1540
Enoch Gyasi <lindakumah03@gmail.com>
2021-08-17 13:54:37 +0000

    CopyCat Project

---
+++ src/components/CopyCat.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {styles} from '../styles';
+import PropTypes from 'prop-types';
+
+const images = {
+  copycat: 'https://content.codecademy.com/courses/React/react_photo_copycat.png',
+  quietcat: 'https://content.codecademy.com/courses/React/react_photo_quietcat.png'
+};
+
+
+export class CopyCat extends React.Component {
+  render() {
+    const {copying, toggleTape, value, handleChange, name} = this.props;
+    
+    return (
+      <div style={styles.divStyles}>
+        <h1 style={{marginBottom:80}}>Copy Cat {name || "Tom"}</h1>
+        <input 
+        type='text' 
+        value={value} 
+        onchange={handleChange}/>
+        <img 
+          alt='cat'
+          src={copying ? images.copycat : images.quietcat}
+          onClick={toggleTape}
+          style={styles.imgStyles}
+        />
+        <p>{copying && value}</p>
+      </div>
+    );
+  };
+}
+
+CopyCat.PropTypes = {
+  copying: PropTypes.bool.isRequired,
+  toggleChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired 
+}
